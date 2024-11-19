575374c6f4dafe412fec73f45e43be241242b475
David-Chazoule <d.chazoule@gmail.com>
2024-11-19 21:38:51 +0100

    recuperation data inside cart, start design

--- productlist/src/components/cart/Cart.tsx
+++ productlist/src/components/cart/Cart.tsx
@@ -1,26 +1,44 @@
-import emptyImg from '../../styles/icons/illustration-empty-cart.svg'
-
+import emptyImg from "../../styles/icons/illustration-empty-cart.svg";
+import { useCart } from "../../Context/CartContext";
 function Cart() {
+  const { cart } = useCart();
+
   return (
     <div className="cart">
-    <div className="cart-quantity">
-    <h3>Your Cart (0)</h3>
-    
+      <div className="cart-quantity">
+        <h3>Your Cart (0)</h3>
+      </div>
 
+      <div className="cart-detail">
+        {cart.length > 0 ? (
+          <div className="cart-detail-information">
+            {cart &&
+              cart.map((item, index) => (
+                <div className="card">
+                  <div className="card-detail">
+                    <span className="title">
+                      <h3>{item.name}</h3>
+                    </span>
+                    <span className="price-detail">
+                      <p>x{item.quantity}</p>
+                      <p>@ $ {item.price}</p>
+                    </span>
+                  </div>
+                  <div className="btn-delete-box">
+                    <span>X</span>
+                  </div>
+                </div>
+              ))}
+          </div>
+        ) : (
+          <div className="cart-empty">
+            <img src={emptyImg} alt="" />
+            <p>Your added items will appear here</p>
+          </div>
+        )}
+      </div>
     </div>
-
-    <div className="cart-detail">
-     <div className="cart-empty">
-      <img src={emptyImg}alt=""/>
-      <p>Your added items will appear here</p>
-     </div>
-
-
-    </div>
-
-
-    </div>
-  )
+  );
 }
 
-export default Cart
+export default Cart;
